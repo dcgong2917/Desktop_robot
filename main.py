63a4efaf2e92ad81cb582cdf7b33687129cbbacb
dechao_gong <dechao_gong@realsil.com.cn>
2026-09-21 16:29:46 +0800

    chore: project scaffold

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+
+
+def main():
+    app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
+    print("App started")
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
